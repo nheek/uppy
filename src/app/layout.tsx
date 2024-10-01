74c93712c7193b8ed5a0ee94d52181f34405bc23
nheek <nickjameshipol@gmail.com>
2024-10-01 22:56:17 +0200

    fixed layout meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,4 @@
-import type { Metadata } from "next";
 import "./globals.css";
-
-export const metadata: Metadata = {
-  title: "Uppy",
-  description: "File hosting/sharing web app",
-};
 
 export default function RootLayout({
   children,
@@ -16,8 +10,8 @@ export default function RootLayout({
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content={metadata.description} />
-        <title>{metadata.title}</title>
+        <meta name="description" content="File hosting/sharing platform" />
+        <title>Uppy</title>
       </head>
       <body>{children}</body>
     </html>
