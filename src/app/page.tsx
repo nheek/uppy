05428574cba2838169071bd97bf9bbae830eb310
nheek <nickjameshipol@gmail.com>
2024-10-01 23:17:42 +0200

    changed favicon and index page to file upload component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-import Index from "@/app/components/Index/Index";
+"use client";
+
 import Header from "@/app/components/Header/Header";
 import React from "react";
+import FileUpload from "@/app/components/FileUpload/FileUpload";
 
 export default function IndexHome() {
   return (
     <>
       <Header />
-      <Index />
+      <FileUpload />
     </>
   );
 }
