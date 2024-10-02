a27b415a6eed4a3af39badef24480b4709959bfd
nheek <nickjameshipol@gmail.com>
2024-10-02 18:55:34 +0200

    Refactor MyFiles component to remove trailing slash from websiteUrl

--- src/app/components/MyFiles/MyFiles.tsx
+++ src/app/components/MyFiles/MyFiles.tsx
@@ -24,7 +24,7 @@ const MyFiles = () => {
   const [modalMessage, setModalMessage] = useState("");
   const [isDeleteConfirmationVisible, setIsDeleteConfirmationVisible] = useState(false);
   const [fileToDelete, setFileToDelete] = useState<File | null>(null);
-  const websiteUrl = `${window.location.protocol}//${window.location.host}/`;
+  const websiteUrl = `${window.location.protocol}//${window.location.host}`;
 
   const showModal = (title: string, message: string) => {
     setModalTitle(title);
